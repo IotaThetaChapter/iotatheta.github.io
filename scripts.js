3617065cce8a369a5e16d5003c32e8d8cdf94602
Iota Theta <ChancelonRice5505@gmail.com>
2025-04-10 11:26:10 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-console.log('JS loaded');
+// Dark Mode Toggle Logic
+const darkToggle = document.getElementById("darkToggle");
+
+if (darkToggle) {
+  darkToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+  });
+}
